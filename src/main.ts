165cd281dd1172cde68f7141346eb5382e4e9ecb
Eduardo Gonzalez <eduardgp@gmail.com>
2020-05-28 22:32:24 +0200

    [FIX] Remove cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, { cors: false });
 	const options = new DocumentBuilder()
 		.setTitle('Bumblebee API')
 		.setDescription('The Bumblebee API description endpoints')

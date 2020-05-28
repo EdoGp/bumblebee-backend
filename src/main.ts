8a36bc64ea4ac3eeec447640a6fca77d2cb603fe
Eduardo Gonzalez <eduardgp@gmail.com>
2020-05-28 22:35:23 +0200

    [FIX] Cors origin

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as helmet from 'helmet';
+// import * as helmet from 'helmet';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, { cors: false });
+	const app = await NestFactory.create(AppModule, { cors: true });
 	const options = new DocumentBuilder()
 		.setTitle('Bumblebee API')
 		.setDescription('The Bumblebee API description endpoints')
 		.setVersion('0.1.0')
 		.build();
+	app.enableCors({ origin: '*' });
 	// app.use(helmet());
 
 	const document = SwaggerModule.createDocument(app, options);
